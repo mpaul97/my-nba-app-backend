eb025864044526b4edcfae5e67b2fa8b11fa89dc
Michael Paul <michaelpaul@Michaels-MacBook-Pro.local>
2025-04-21 12:22:52 -0500

    odds

---
+++ odds/odds_api_testing.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.environ['API_URL']
+API_KEY = os.environ['API_KEY']
+
+res = requests.get(
+    url=f"{API_URL}/historical/sports/basketball_nba/odds",
+    params={
+        'apiKey': API_KEY,
+        'regions': 'us',
+        'date': '2025-04-20T12:15:00Z'
+    }
+)
+
+json.dump(res.json(), open("responses/historical_nba_odds_2025-04-20.json", "w"), indent=4)
