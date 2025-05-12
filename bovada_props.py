77c7077e4a62d50929a38a7e4e75e7bbe09accce
mpaul97 <85245587+mpaul97@users.noreply.github.com>
2025-05-11 21:58:12 -0500

    bovada props

--- bovada_props.py
+++ bovada_props.py
@@ -26,18 +26,14 @@ class PropObj:
         self.item = item
         # set values
         self.player_name: str = self.item['player_name']
-        self.date: datetime = datetime.fromtimestamp(self.item['date']/1000, tz=timezone.utc)
+        self.date: datetime = datetime.fromtimestamp(self.item['bovada_date']/1000)
         self.stat: str = self.item['stat']
         self.line: float = self.item['line_value']
         self.link: str = self.item['link']
         if self.link:
             game_date = self.link.split("-")[-1]
             self.date = datetime.strptime(game_date, "%Y%m%d%H%M")
-        try:
-            self.player_data = find_players_by_full_name(self.player_name)[0]
-        except:
-            logging.error(f"No player data found for: {self.player_name}")
-            return
+        self.player_id = self.item['player_id']
         return
 
 class Props:
@@ -51,18 +47,22 @@ class Props:
         for item in data:
             po = PropObj(item)
             if po.date < datetime.now():
-                if po.date not in self.all_dates:
-                        self.all_dates.append(po.date)
+                if po.date.date() not in self.all_dates:
+                        self.all_dates.append(po.date.date())
                 self.all_props.append(po)
         if any([len(self.all_props)==0, len(self.all_dates)==0]):
             logging.error("No games or dates found for props.")
             return
-        self.boxscores: list[dict] = self.get_boxscores()
+        self.boxscores: dict = {}
+        self.set_boxscores()
         if save_boxscores:
-            for vals in self.boxscores:
-                vals['frame'].to_csv(f"./data/{vals['date'].strftime("%Y-%m-%d")}_{vals['game_id']}_boxscore.csv", index=False)
+            for key in self.boxscores:
+                self.boxscores[key].to_csv(f"./data/{key.strftime("%Y-%m-%d")}_boxscores.csv", index=False)
         outcomes: list[dict] = self.get_bet_hits()
-        pd.DataFrame(outcomes).drop(columns=['link']).to_csv(f"outcomes.csv", index=False)
+        new_df: pd.DataFrame = pd.DataFrame(outcomes)
+        new_df = new_df.sort_values(by=['bovada_date', 'team_abbr', 'player_name'], ascending=False)
+        new_df.insert(0, 'date', new_df['bovada_date'].apply(lambda x: datetime.fromtimestamp(x/1000).strftime("%Y-%m-%d")))
+        new_df.drop(columns=['bovada_date', 'link']).to_csv(f"outcomes.csv", index=False)
         return
     def print_progress_bar(self, iteration, total, prefix = 'Progress', suffix = 'Complete', decimals = 1, length = 50, fill = '█', printEnd = "\r"):
         """
@@ -85,32 +85,27 @@ class Props:
         if iteration == total: 
             print()
         return
-    def get_boxscore_data(self, data: list, dates: list[datetime], just_frame: bool = False):
+    def fetch_boxscore(self, game_id: str):
+        box_score = BoxScoreTraditionalV3(game_id=game_id)
+        return box_score.get_data_frames()[0]
+    def get_boxscore_data(self, dates: list[datetime]):
         for date in dates:
-            s_date: str = date.strftime("%Y-%m-%d")
-            games = ScoreboardV2("0", s_date, "00")
-            gdf: pd.DataFrame = games.get_data_frames()[0]
-            ids: list = list(gdf['GAME_ID'].values)
-            for id in ids:
-                try:
-                    df: pd.DataFrame = BoxScoreTraditionalV3(id).get_data_frames()[0]
-                    data.append({ "date": date, "game_id": id, "frame": df })
-                    time.sleep(0.6)
-                except Exception as e:
-                    logging.error(f"Game not found (possible not played yet): {id}")
-        if just_frame:
-            return pd.concat([d['frame'] for d in data])
-        return data
-    def get_boxscores(self):
-        data = []
+            games = ScoreboardV2(game_date=date.strftime("%Y-%m-%d"))
+            game_ids: list[str] = list(games.get_data_frames()[0]['GAME_ID'].values)
+            self.boxscores[date] = pd.DataFrame()
+            for _id in game_ids:
+                self.boxscores[date] = pd.concat([self.boxscores[date], self.fetch_boxscore(_id)])
+                time.sleep(0.6)
+        return
+    def set_boxscores(self):
         if self.load_boxscores: # load
             for fn in os.listdir("./data/"):
-                if re.search(r"_boxscore", fn):
-                    data.append({ "date": datetime.strptime(fn.split("_")[0], "%Y-%m-%d"), "game_id": fn.split("_")[1], "frame": pd.read_csv(f"./data/{fn}") })
-            return data
+                if re.search(r"_boxscores", fn):
+                    self.boxscores[datetime.strptime(fn.split("_")[0], "%Y-%m-%d").date()] = pd.read_csv(f"./data/{fn}")
+            return
         # fetch
-        data = self.get_boxscore_data(data, self.all_dates)
-        return data
+        self.get_boxscore_data(self.all_dates)
+        return
     def get_bet_hits(self):
         outcomes = []
         po: PropObj
@@ -119,19 +114,16 @@ class Props:
             self.print_progress_bar(index, len(self.all_props))
             try:
                 try:
-                    df: pd.DataFrame = [
-                        b['frame'] for b in self.boxscores \
-                        if b['date'].date()==po.date.date() and po.player_data['id'] and \
-                        po.player_data['id'] in b['frame']['personId'].values
-                    ][0]
+                    df: pd.DataFrame = self.boxscores[po.date.date()]
                 except IndexError:
-                    print(f"Boxscore not fround for exact prop date, trying day before: {po.date.date()}")
-                    df: pd.DataFrame = self.get_boxscore_data(
-                        data=[],
-                        dates=[po.date-timedelta(days=1)],
-                        just_frame=True
-                    )
-                df: pd.DataFrame = df[df['personId']==po.player_data['id']]
+                    print("No frame found!")
+                    # print(f"Boxscore not fround for exact prop date, trying day before: {po.date.date()}")
+                    # df: pd.DataFrame = self.get_boxscore_data(
+                    #     data=[],
+                    #     dates=[po.date-timedelta(days=1)],
+                    #     just_frame=True
+                    # )
+                df: pd.DataFrame = df[df['personId']==po.player_id]
                 game_vals = df[BOVADA_BOXSCORE_MAPPINGS[po.stat]].values[0]
                 actual_total = int(sum(game_vals))
                 new_item = po.item.copy()
@@ -154,5 +146,6 @@ class Props:
 
 if __name__=="__main__":
     data: list[dict] = json.load(open("all_player_props_nba.json", "r"))
-    Props(data, save_boxscores=True)
-    # Props(data, load_boxscores=True)
+    # data = random.sample(data, k=50)
+    # Props(data, save_boxscores=True)
+    Props(data, load_boxscores=True)
